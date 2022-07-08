b8cab605f84a8846b947549cc69cd38bd5a0c752
Valerii Shamota <v.shamota@kennersoft.de>
2022-07-08 15:21:30 +0300

    Fixed crons

--- app/Treo/Console/Cron.php
+++ app/Treo/Console/Cron.php
@@ -92,12 +92,12 @@ class Cron extends AbstractConsole
 
         // open daemon queue manager stream 0
         if (empty(strpos($processes, "index.php daemon qm 0-$id"))) {
-//            exec("$php index.php daemon qm 0-$id >/dev/null 2>&1 &");
+            exec("$php index.php daemon qm 0-$id >/dev/null 2>&1 &");
         }
 
         // open daemon queue manager stream 1
         if (empty(strpos($processes, "index.php daemon qm 1-$id"))) {
-//            exec("$php index.php daemon qm 1-$id >/dev/null 2>&1 &");
+            exec("$php index.php daemon qm 1-$id >/dev/null 2>&1 &");
         }
 
         // open daemon notification
