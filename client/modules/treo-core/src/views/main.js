a968c8a7b96ee1609ca46da868f45c6659ece696
Valerii Shamota <v.shamota@kennersoft.de>
2025-01-31 16:07:04 +0200

    fix header after save

--- client/modules/treo-core/src/views/main.js
+++ client/modules/treo-core/src/views/main.js
@@ -39,12 +39,6 @@ Espo.define('treo-core:views/main', 'class-replace!treo-core:views/main', functi
 
     return Dep.extend({
 
-        setupFinal() {
-            Dep.prototype.setupFinal.call(this);
-
-            this.bindFixedHeaderOnScroll();
-        },
-
         buildHeaderHtml: function (arr) {
             var a = [];
             arr.forEach(function (item) {
@@ -56,31 +50,6 @@ Espo.define('treo-core:views/main', 'class-replace!treo-core:views/main', functi
                 fixedHeader = 'fixed-header-breadcrumbs';
             }
             return '<div class="header-breadcrumbs ' + fixedHeader + '">' + a.join('<span class="breadcrumb-separator"> &rsaquo; </span>') + '</div>';
-        },
-
-        bindFixedHeaderOnScroll() {
-            let $window = $(window);
-            this.listenToOnce(this, 'remove', () => {
-                $window.off('scroll.fixed-header')
-            });
-            this.listenTo(this, 'after:render', () => {
-                $window.off('scroll.fixed-header');
-                $window.on('scroll.fixed-header', () => {
-                    let scrollTop = $window.scrollTop();
-                    let header = this.$el.find('.header-breadcrumbs');
-                    let navBarRight = $('#header .navbar-right');
-                    let width = $('#header ul.navbar-right > li').get().reduce((prev, curr) => {
-                        return prev - $(curr).outerWidth()
-                    }, navBarRight.outerWidth() - 30);
-                    if (scrollTop > this.$el.find('.page-header').outerHeight() && !$('#header .navbar .menu').hasClass('open-menu')) {
-                        header.addClass('fixed-header-breadcrumbs')
-                            .css('width', width + 'px');
-                    } else {
-                        header.removeClass('fixed-header-breadcrumbs')
-                            .css('width', 'auto');
-                    }
-                });
-            });
         }
 
     });
